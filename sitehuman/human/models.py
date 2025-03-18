6652851a374a416e829a28425369d0947d6ac885
Vladimiv Ivanov <ivs180397@yandex.ru>
2025-03-18 18:03:57 +0300

    my_birthday

--- sitehuman/human/models.py
+++ sitehuman/human/models.py
@@ -48,7 +48,7 @@ class Human(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Предназначения")
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Имя")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Slug")
 
     def __str__(self):
         return self.name
